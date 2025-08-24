7d2a036b983ede39d042c21eef12e1366a0c664a
malak887 <malakawny0808@gmail.com>
2025-08-24 04:39:30 +0300

    add Weopon class definition header in characters.h

--- characters.h
+++ characters.h
@@ -1,6 +1,8 @@
 #ifndef CHARACTERS_H   
 #define CHARACTERS_H
-
+#include <iostream>
+#include <cstdlib>  
+#include <ctime>  
 #include <string>
 using namespace std;
 
@@ -27,5 +29,17 @@ Robots(string name);
 
 int get_health();
 string get_name();
+};
+
+
+class Weopon {
+private:
+int damage;
+int accuracy;
+public:
+string name;
+
+Weopon (string name ,int demage,int accuracy){};
+bool shot (int accuracy){};
 };
 #endif
