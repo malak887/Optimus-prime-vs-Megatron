7e176c8c8b1f0d28ee92a4bbcb7fe7d4ff3ddf88
malak887 <malakawny0808@gmail.com>
2025-08-24 05:37:16 +0300

    remove unused get_health method and clean up constructor in Characters class

--- characters.cpp
+++ characters.cpp
@@ -1,14 +1,11 @@
 #include "characters.h"
 #include <iostream>
 using namespace std;
+ Characters::Characters(){}
 
 Characters::Characters(string name) {
 this->name=name;
    
-}
-
-int Characters::get_health() {
-   return health;
 }
 
 string Characters::get_name() {
