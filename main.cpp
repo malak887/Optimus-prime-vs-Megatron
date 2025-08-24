d825c69af79406dce2813e1ab0ce042abcc402c9
malak887 <malakawny0808@gmail.com>
2025-08-24 12:26:20 +0300

    fix: refactor weapon display logic and improve turn handling in main game loop

--- main.cpp
+++ main.cpp
@@ -2,19 +2,69 @@
 #include "characters.h"
 #include "Weopon.h"
 using namespace std;
+void display_weopons(Robots r){
+    cout<<r.get_name()<<"'s Weopons:"<<endl;
+    for(int i=0;i<3;i++){
+        cout<<i+1<<". "<<r.w[i].get_name()<<" (Damage: "<<r.w[i].get_demage()<<", Accuracy: "<<r.w[i].get_accuracy()<<"%)"<<endl;
+    }
+}
+void display_health(Robots r1){
+    cout<<r1.get_name()<<"'s health: "<<r1.get_health()<<endl;
+  
+}
+bool ifloser(Robots r1, Robots r2){
+    if(r1.get_health()<=0){
+        cout<<r1.get_name()<<" has been defeated!"<<endl;
+        cout<<r2.get_name()<<" wins the battle!"<<endl;
+        return true;
+    }
+    return false;}
 int main() { 
     Robots Optimus("Optimus Prime", Weopon("Ion rifl", 6, 100), Weopon("Energon swords", 12, 80), Weopon("Shoulder canon", 45, 25));
     Robots Megatron("Megatron", Weopon("Fusion cannon", 9, 90), Weopon("Tank Mode", 60, 15), Weopon("Fusion Sword", 60, 15));
    cout<<"Welcome to the battle between Optimus Prime and Megatron!"<<endl;
    cout<<"Each robot has 3 weopons to choose from:"<<endl; 
-    cout<<"1. "<<Optimus.w[0].get_name()<<" (Damage: "<<Optimus.w[0].get_demage()<<", Accuracy: "<<Optimus.w[0].get_accuracy()<<"%)"<<endl;
-    cout<<"2. "<<Optimus.w[1].get_name()<<" (Damage: "<<Optimus.w[1].get_demage()<<", Accuracy: "<<Optimus.w[1].get_accuracy()<<"%)"<<endl;
-    cout<<"3. "<<Optimus.w[2].get_name()<<" (Damage: "<<Optimus.w[2].get_demage()<<", Accuracy: "<<Optimus.w[2].get_accuracy()<<"%)"<<endl;
+    cout<<"Optimus Primes Weopons:"<<endl;
+   display_weopons(Optimus);
     cout<<"-----------------------------------------------"<<endl;
-    cout<<"1. "<<Megatron.w[0].get_name()<<" (Damage: "<<Megatron.w[0].get_demage()<<", Accuracy: "<<Megatron.w[0].get_accuracy()<<"%)"<<endl;
-    cout<<"2. "<<Megatron.w[1].get_name()<<" (Damage: "<<Megatron.w[1].get_demage()<<", Accuracy: "<<Megatron.w[1].get_accuracy()<<"%)"<<endl;
-    cout<<"3. "<<Megatron.w[2].get_name()<<" (Damage: "<<Megatron.w[2].get_demage()<<", Accuracy: "<<Megatron.w[2].get_accuracy()<<"%)"<<endl;
+    cout<<"Optimus Primes Weopons:"<<endl;
+    display_weopons(Megatron);
     cout<<"-----------------------------------------------"<<endl;  
-return 0;
+    bool turn=true; //true for optimus, false for megatron
+    while(Optimus.get_health()>0 && Megatron.get_health()>0){
+        if(turn){
+            int choice;
+            cout<<Optimus.get_name()<<"'s turn."<<endl;
+            display_health(Optimus);
+          cout<<  " Choose a weopon (1-3): "<<endl;
+             display_weopons(Optimus);
+            cin>>choice;
+            if(choice<1 || choice>3){
+                cout<<"Invalid choice. Try again."<<endl;
+                continue;
+            }
+       Optimus.attack(Megatron, Optimus.w[choice-1]);
+            if(  ifloser(Megatron, Optimus))break;
+          
+        }
+        else{
+            int choice;
+            cout<<Megatron.get_name()<<"'s turn."<<endl;
+            display_health(Megatron);
+            cout<<  " Choose a weopon (1-3): "<<endl;
+             display_weopons(Megatron);
+            cin>>choice;
+            if(choice<1 || choice>3){
+                cout<<"Invalid choice. Try again."<<endl;
+                continue;
+            }
+            Megatron.attack(Optimus,Megatron.w[choice-1]);
+             if(  ifloser(Optimus, Megatron))break;
 
+        }
+           turn =!turn;
+}
+     
+  
+    return 0;
 }
